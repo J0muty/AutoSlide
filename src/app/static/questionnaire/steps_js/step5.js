19306031326c869e9d69e4fa6417aff5db01a0b9
Христофор <168688308+J0muty@users.noreply.github.com>
2025-06-26 01:34:58 +0300

    5 step

--- src/app/static/questionnaire/steps_js/step5.js
+++ src/app/static/questionnaire/steps_js/step5.js
@@ -1,1 +1,11 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+    const options = document.querySelectorAll('.image-style-option');
+    const preview = document.querySelector('.image-style-preview img');
+
+    options.forEach((option) => {
+        option.addEventListener('click', () => {
+            options.forEach((el) => el.classList.remove('active'));
+            option.classList.add('active');
+        });
+    });
+});
