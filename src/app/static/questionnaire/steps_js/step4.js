ffa997588b4e03a1ccd647a87fdf5a71bac1e08f
Христофор <168688308+J0muty@users.noreply.github.com>
2025-06-26 01:08:03 +0300

    4 степ сделанный будто красиво визуально но опять же без функционала пока что

--- src/app/static/questionnaire/steps_js/step4.js
+++ src/app/static/questionnaire/steps_js/step4.js
@@ -1,1 +1,17 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+    const styleBtns = document.querySelectorAll('.step[data-step="4"] .style-btn');
+    styleBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            styleBtns.forEach(b => b.classList.remove('active'));
+            btn.classList.add('active');
+        });
+    });
+
+    const themeCards = document.querySelectorAll('.step[data-step="4"] .theme-card');
+    themeCards.forEach(card => {
+        card.addEventListener('click', () => {
+            themeCards.forEach(c => c.classList.remove('active'));
+            card.classList.add('active');
+        });
+    });
+});
